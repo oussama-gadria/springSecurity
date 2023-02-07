b2d3f38b6b5010391884cd889e31cd8af15537dd
Oussama Gadria <gadria.oussama@esprit.tn>
2023-02-07 01:02:04 +0100

    Implement Authentication controller

--- src/main/java/com/workshop/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/workshop/springSecurity/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.workshop.springSecurity.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,6 +65,10 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
        // return new BCryptPasswordEncoder();
         return NoOpPasswordEncoder.getInstance(); //not crypted
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws  Exception{
+        return config.getAuthenticationManager();
     }
     @Bean
     public UserDetailsService userDetailsService(){
